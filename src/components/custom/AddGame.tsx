ee7ac12509c6a24206463df2041949d0edf3c5d7
iamzainn <iamzainnf@gmail.com>
2024-03-29 00:12:07 +0500

    fin

--- src/components/custom/AddGame.tsx
+++ src/components/custom/AddGame.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React, {  useEffect, useRef, useState } from 'react'
 
+
+
 import { useQuery } from '@tanstack/react-query';
 import {
     Dialog,
@@ -30,6 +32,19 @@ import { teamType } from './GetTeams';
 
 import CalendarForm from './Date';
 import { toast } from '../ui/use-toast';
+import { number } from 'zod';
+
+type postedGameData = {
+  sportId:number|null
+  leagueId:number|null
+  teamId1: number|null
+  teamId2 :number|null
+  date:Date|undefined
+  links:string[]
+  venue:string,
+
+}
+
 
 const AddGame = () => {
     const [sports,setSports] = useState([] as SportData[])
@@ -43,8 +58,60 @@ const AddGame = () => {
     const [date,setDate] = useState<Date | undefined>(); 
     const [links,setLinks]=useState<string[]>([]);
     const [showBtn,setShowBtn] = useState(false);
-    const venue = useRef<HTMLInputElement>(null!)
+    const [venue,setVenue] = useState("");
     const [value,setValue]= useState("");
+
+         
+    const reset = ()=>{
+      setSportId(null);
+      setLeagueId(null);
+      setTeamId1(null);
+      setTeamId2(null);
+      setValue("");
+      setDate(undefined);
+      setVenue("")
+     setShowBtn(false);
+     setLinks([]);
+    }
+
+  
+    const postGame = async(data:postedGameData)=>{
+      try{
+        setLoading(true);
+          const res=await fetch('/api/auth/games',{
+          method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+         })
+         if(res.ok){
+          const data = await res.json();
+          toast({
+            description: data.message,
+          })
+          reset();
+          // client.invalidateQueries({ queryKey:["GET_TEAMS"] })
+          
+         }
+         if(!res.ok){
+          toast({
+            description: JSON.stringify(await res.json())
+          })
+         }
+         reset();
+      }catch(e){
+        toast({
+          description: (e as Error).message,
+        })
+        reset();
+       console.error(e);
+      }
+      finally{
+        setLoading(false);
+        reset();
+      }
+   }
 
 
     
@@ -53,14 +120,9 @@ const AddGame = () => {
 
     
     
-useEffect(()=>{
+
    
-  if(date){
-    toast({
-      description: (<p>{JSON.stringify(date.toLocaleDateString() )} is added</p>),
-    })
-  }
-},[date])
+ 
     
      useEffect(()=>{
       if(sportId){
@@ -166,9 +228,11 @@ useEffect(()=>{
       }
 
 
-    const handleClick =(value:any)=>{
-    
-
+    const handleClick =()=>{
+     
+      
+      postGame({sportId,leagueId,links,teamId1,teamId2,date,venue})
+     
     }
   return (
     <>
@@ -243,8 +307,8 @@ useEffect(()=>{
           </div>
           
          {sportId && leagueId && teamId1 && teamId2 ?<div className="flex flex-wrap gap-3 items-center justify-center">
-            <Input type='text'placeholder='Match Venue' ref={venue} className=''></Input>
-         <div className=''>  <CalendarForm setDate = {setDate} ></CalendarForm></div>
+            <Input type='text'placeholder='Match Venue' value={venue} onChange={(e)=>setVenue(e.target.value)} className=''></Input>
+         <div className=''>  <CalendarForm setDate = {setDate} date={date} ></CalendarForm></div>
          <div className='links flex flex-col'>
           <Input type='text' value={value} onChange={addLink} placeholder='Add Streaming links' id="link"></Input>
           {showBtn ?<Button className='mt-4' onClick={()=>{handleAdd(value)}}>Add link</Button>:null}
@@ -253,7 +317,7 @@ useEffect(()=>{
          
         </div>
         <DialogFooter>
-          <Button type="submit" disabled={ (venue==null) ||(!sportId) || (!leagueId) || (!teamId1) || (!teamId2)  || (!date) || (! (teamId1!==teamId2) || (!links.length))} onClick={handleClick} >{loading?"Loading":"Add Game"}</Button>
+          <Button type="submit" disabled={ (!venue) ||(!sportId) || (!leagueId) || (!teamId1) || (!teamId2)  || (!date) || (! (teamId1!==teamId2) || (!links.length))} onClick={handleClick} >{loading?"Loading":"Add Game"}</Button>
         </DialogFooter>
       </DialogContent>
       
